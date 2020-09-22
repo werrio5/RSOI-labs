a924be929ff01322f6c0f0cd6028bd21653541ec
werrio5 <55055904+werrio5@users.noreply.github.com>
2020-09-22 19:33:32 +0300

    Add files via upload

---
+++ docker-lab/dockerApp/src/main/java/com/mycompany/dockerapp/MainClass.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.dockerapp;
+
+/**
+ *
+ * @author werrio5
+ */
+public class MainClass {
+    public static void main(String[] args) {
+        System.out.println("an application for execution inside docker");
+        System.out.println("123");
+        System.out.println("qwe");
+    }
+}
