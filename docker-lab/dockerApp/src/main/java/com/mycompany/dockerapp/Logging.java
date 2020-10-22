232cbd9a6206020a8adc878e681244c32f98f06f
pavel <pavel@PC2>
2020-10-22 20:01:09 +0300

    java http server

---
+++ docker-lab/dockerApp/src/main/java/com/mycompany/dockerapp/Logging.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.dockerapp;
+
+/**
+ *
+ * @author pavel
+ */
+public class Logging {
+    
+    public static void log(){
+        
+    }
+}
