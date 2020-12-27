5ce1be65344a4f9d4b6dbff3666b8c6e2a97920a
werrio5 <55055904+werrio5@users.noreply.github.com>
2020-12-27 23:35:17 +0300

    Update startup.sh

--- load-balancer/VM-2/startup.sh
+++ load-balancer/VM-2/startup.sh
@@ -1,3 +1,22 @@
 #!/bin/sh
-export EXTERNAL_IP=$(ip addr show enp0s3 | grep -Po 'inet \K[\d.]+')
-exec docker-compose $@
+EXTERNAL_IP=$(ip addr show enp0s3 | grep -Po 'inet \K[\d.]+')
+echo "self" $EXTERNAL_IP
+export EXTERNAL_IP
+
+CONSUL_URL="consul://192.168.1.8:8500"
+echo "consul" $CONSUL_URL
+export CONSUL_URL
+
+#port
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind(('', 0))
+addr = s.getsockname()
+echo "port" addr[1]
+PORT=addr[1]
+s.close()
+
+export PORT
+
+docker-compose up
