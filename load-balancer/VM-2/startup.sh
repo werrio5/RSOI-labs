585d1045af7b6c4391456a15f13f9d44201d46e5
werrio5 <55055904+werrio5@users.noreply.github.com>
2021-01-19 16:13:42 +0300

    Update startup.sh

--- load-balancer/VM-2/startup.sh
+++ load-balancer/VM-2/startup.sh
@@ -7,23 +7,4 @@ CONSUL_URL="consul://192.168.1.8:8500"
 echo "consul" $CONSUL_URL
 export CONSUL_URL
 
-#port
-
-PORT=0
-for i in {50000..60000}
-do
-  PORT=$i
-  RES=$(ss -ln src :$PORT | grep $PORT)
-  if [ "${RES}" = "" ]; then
-    break    
-  fi
-done
-
-echo "port " $PORT
-export PORT
-
-reg_started=$(docker ps | grep registrator)
-if [ "${reg_started}" = "" ]; 
-then docker-compose up
-else docker-compose run -p $PORT:8080 app
-fi
+docker-compose up
